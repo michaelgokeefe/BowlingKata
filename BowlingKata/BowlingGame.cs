b35be61e00ba099eceec8ba908dc00e227b785df
MichaelGeraldOKeefe <mgok@uw.edu>
2017-06-01 13:37:04 -0700

    Implementation 1 finished

--- BowlingKata/BowlingGame.cs
+++ BowlingKata/BowlingGame.cs
@@ -6,7 +6,68 @@ using System.Threading.Tasks;
 
 namespace BowlingKata
 {
-    public class Class1
+    public class BowlingGame
     {
+        private int rollIndex = 0;
+        private int[] rolls = new int[21];
+       
+        public void Roll(int numberOfPins)
+        {
+            if (numberOfPins < 0 || numberOfPins > 10)
+                throw new ArgumentOutOfRangeException();
+
+            rolls[rollIndex++] = numberOfPins;
+        }
+
+        public int CalculateScore()
+        {
+            int score = 0;
+            int roll = 0; 
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(roll))
+                {
+                    score += 10 + GetStrikeBonus(roll);
+                    roll++;
+                }
+                else if (IsSpare(roll))
+                {
+                    score += 10 + GetSpareBonus(roll);
+                    roll += 2;
+                }
+                else
+                {
+                    score += GetPinsKnockedDownInFrame(roll);
+                    roll += 2;
+                }
+            }
+            return score;
+        }
+
+        private bool IsSpare(int roll)
+        {
+            return rolls[roll] + rolls[roll + 1] == 10;
+        }
+
+        private bool IsStrike(int roll)
+        {
+            return rolls[roll] == 10;
+        }
+
+        private int GetStrikeBonus(int roll)
+        {
+            return rolls[roll + 1] + rolls[roll + 2];
+        }
+
+        private int GetSpareBonus(int roll)
+        {
+            return rolls[roll + 2];
+        }
+
+        private int GetPinsKnockedDownInFrame(int roll)
+        {
+            return rolls[roll] + rolls[roll + 1];
+        }
     }
 }
--- BowlingTests/BowlingTests.cs
+++ BowlingTests/BowlingTests.cs
@@ -1,14 +1,149 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingKata;
 
 namespace BowlingTests
 {
     [TestClass]
-    public class UnitTest1
+    public class BowlingTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private BowlingGame bowlingGame;
+
+        [TestInitialize]
+        public void Initialize()
         {
+            bowlingGame = new BowlingGame();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            bowlingGame = null;
+        }
+
+        [TestMethod]
+        public void CanCallRoll()
+        {
+            bowlingGame.Roll(1);
+        }
+
+        [TestMethod]
+        public void CanCallCalculateScore()
+        {
+            bowlingGame.CalculateScore();
+        }
+
+        [TestMethod]
+        public void StartingScoreIsZero()
+        {
+            Assert.AreEqual(0, bowlingGame.CalculateScore());
+        }
+
+        [TestMethod]
+        public void RollingAddsToScore()
+        {
+            bowlingGame.Roll(1);
+            bowlingGame.Roll(3);
+            Assert.AreEqual(4, bowlingGame.CalculateScore());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CannotRollNegativePins()
+        {
+            bowlingGame.Roll(-1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CannotRollOverTenPins()
+        {
+            bowlingGame.Roll(11);
+        }
+
+        [TestMethod]
+        public void CanCalculateAllZeroGame()
+        {
+            RollMany(0, 20);
+            Assert.AreEqual(0, bowlingGame.CalculateScore());
+        }
+        
+        [TestMethod]
+        public void TestAllOnes()
+        {
+            RollMany(1, 20);
+            Assert.AreEqual(20, bowlingGame.CalculateScore());
+        }
+
+        [TestMethod]
+        public void CanRollAGameWithoutSparesOrStrikes()
+        {
+            bowlingGame.Roll(1);
+            bowlingGame.Roll(7);
+            bowlingGame.Roll(9);
+            bowlingGame.Roll(0);
+            bowlingGame.Roll(5);
+            RollMany(0, 15);
+            Assert.AreEqual(22, bowlingGame.CalculateScore());
+        }
+
+        [TestMethod]
+        public void CanRollAGameWithOneSpare()
+        {
+            bowlingGame.Roll(1);
+            bowlingGame.Roll(9);
+            bowlingGame.Roll(8);
+            Assert.AreEqual(26, bowlingGame.CalculateScore());
+        }
+
+        [TestMethod]
+        public void CanRollAGameWithOneStrike()
+        {
+            bowlingGame.Roll(10);
+            bowlingGame.Roll(5);
+            bowlingGame.Roll(6);
+            Assert.AreEqual(32, bowlingGame.CalculateScore());
+        }
+
+        [TestMethod]
+        public void CanRollAVariedGameWithStrikesAndSpares()
+        {
+            bowlingGame.Roll(1);
+            bowlingGame.Roll(4);
+            bowlingGame.Roll(4);
+            bowlingGame.Roll(5);
+            bowlingGame.Roll(6);
+            bowlingGame.Roll(4);
+            bowlingGame.Roll(5);
+            bowlingGame.Roll(5);
+            bowlingGame.Roll(10);
+            bowlingGame.Roll(0);
+            bowlingGame.Roll(1);
+            bowlingGame.Roll(7);
+            bowlingGame.Roll(3);
+            bowlingGame.Roll(6);
+            bowlingGame.Roll(4);
+            bowlingGame.Roll(10);
+            bowlingGame.Roll(2);
+            bowlingGame.Roll(8);
+            bowlingGame.Roll(6);
+            Assert.AreEqual(133, bowlingGame.CalculateScore());
+
+        }
+
+        [TestMethod]
+        public void TestPerfectGame()
+        {
+            RollMany(10, 12);
+            Assert.AreEqual(300, bowlingGame.CalculateScore());
+        }
+
+        private void RollMany(int numberOfPins, int numberOfRolls)
+        {
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                bowlingGame.Roll(numberOfPins);
+            }
         }
     }
 }
